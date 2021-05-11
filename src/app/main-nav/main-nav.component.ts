3d4496fceadfe64a0d075492eb2cb5d69fcbee44
Luis Saldana <tluis10a@gmail.com>
2021-05-11 16:20:04 -0700

    configure routing for the components

--- src/app/main-nav/main-nav.component.ts
+++ src/app/main-nav/main-nav.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-nav',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainNavComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+  }
+
+  navigateTo(path: string) {
+    this.router.navigate([path]);
   }
 
 }
